ca287c36a8a5c3a1b4c897575746a11bd85e1a75
felipefrf <felipe.frf27@gmail.com>
2024-09-12 11:28:53 -0300

    fix: Team Section behavior

--- src/screens/components/Team/ProfileCard.tsx
+++ src/screens/components/Team/ProfileCard.tsx
@@ -1,9 +1,9 @@
-// src/components/ProfileCard.tsx
 import React from "react";
 import { Card, CardContent, Box, Typography, IconButton } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import XIcon from "@mui/icons-material/X";
+import "@fontsource/inter";
 
 interface ProfileCardProps {
   name: string;
@@ -25,42 +25,56 @@ const ProfileCard: React.FC<ProfileCardProps> = ({
   return (
     <Card
       sx={{
-        maxWidth: 300, // Reduced max width
-        borderRadius: "8px", // Reduced border radius
+        minWidth: 250,
+        maxWidth: 300,
+        borderRadius: "20px",
         backgroundColor: "#222325",
         color: "#fff",
-        overflow: "visible", // Allow content to overflow for image
+        overflow: "visible",
+        position: "relative",
       }}
     >
       {/* Image container */}
       <Box
         sx={{
-          height: 200, // Fixed height for image
-          overflow: "hidden",
-          borderTopLeftRadius: "8px",
-          borderTopRightRadius: "8px",
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          top: "-20px", // Reduce the amount the image floats above the card
+          marginBottom: "-20px",
         }}
       >
-        <img
+        <Box
+          component="img"
           src={image}
           alt={name}
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
+          sx={{
+            width: "100%", // Make the image responsive
+            maxWidth: "196px", // Max dimensions to match card
+            height: "auto", // Automatically adjust the height
+            borderRadius: "13px", // Rounded corners
+            objectFit: "cover", // Ensure the image covers the box proportionally
+            aspectRatio: "1/1", // Ensure the image maintains a square aspect ratio
           }}
         />
       </Box>
-      <CardContent sx={{ padding: "16px" }}>
+      <CardContent sx={{ padding: "0" }}>
         <Typography
           variant="h6"
-          sx={{ fontWeight: "bold", fontSize: "1.1rem" }}
+          sx={{ fontWeight: "bold", fontSize: "1.1rem", fontFamily: "Inter" }}
         >
           {name}
         </Typography>
         <Typography
           variant="body2"
-          sx={{ color: "#ccc", fontSize: "0.9rem", marginBottom: "12px" }}
+          sx={{
+            color: "#ccc",
+            fontSize: "0.9rem",
+            marginBottom: "12px",
+            fontFamily: "Inter",
+          }}
         >
           {title}
         </Typography>
@@ -93,7 +107,9 @@ const ProfileCard: React.FC<ProfileCardProps> = ({
             >
               {key === "linkedin" && <LinkedInIcon sx={{ fontSize: "20px" }} />}
               {key === "x" && <XIcon sx={{ fontSize: "20px" }} />}
-              {key === "facebook" && <TelegramIcon sx={{ fontSize: "20px" }} />}
+              {key === "facebook" && (
+                <TelegramIcon sx={{ fontSize: "20px" }} />
+              )}
             </IconButton>
           ))}
         </Box>
--- src/screens/components/TeamSection.tsx
+++ src/screens/components/TeamSection.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import ProfileCard from "./Team/ProfileCard";
+import { Box, Typography, useTheme, useMediaQuery, Grid } from "@mui/material";
+import MyProfileCard from "./Team/ProfileCard";
 import teamMembers from "../../utils/team";
 
 const TeamSection: React.FC = () => {
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isSmallMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md")); // Screen above 900px
 
   return (
     <Box
@@ -17,14 +18,13 @@ const TeamSection: React.FC = () => {
       }}
     >
       <Typography
-        variant={isSmallScreen ? "h4" : "h3"}
+        variant={isSmallMobile ? "h4" : "h3"}
         color="white"
         sx={{
           maxWidth: "800px",
           margin: "0 auto",
           textAlign: "center",
-          paddingLeft: "20px",
-          paddingRight: "20px",
+          padding: "0 20px",
           fontFamily: "Space Grotesk, sans-serif",
         }}
       >
@@ -33,13 +33,12 @@ const TeamSection: React.FC = () => {
       <Typography
         variant="body1"
         sx={{
-          maxWidth: isSmallScreen ? "90%" : "600px",
+          maxWidth: isSmallMobile ? "90%" : "600px",
           margin: "16px auto 0",
           textAlign: "center",
-          fontSize: isSmallScreen ? "0.9rem" : "1rem",
+          fontSize: isSmallMobile ? "0.9rem" : "1rem",
           lineHeight: 1.5,
-          paddingLeft: "20px",
-          paddingBottom: "60px",
+          padding: "0 20px 60px",
           fontFamily: "Space Grotesk, sans-serif",
           color: "#C2CDE7",
         }}
@@ -49,26 +48,73 @@ const TeamSection: React.FC = () => {
       </Typography>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          justifyContent: "center",
-          gap: "60px",
-          padding: { xs: "20px", md: "20px 40px" },
-          maxWidth: "1200px",
-          marginTop: "60px",
-          margin: "0 auto",
+          maxWidth: '1200px',
+          margin: '60px auto 0',
+          padding: { xs: '20px', md: '20px 40px' },
         }}
       >
-        {teamMembers.map((member, index) => (
-          <Box sx={{ gridArea: { md: `card${index + 1}` } }} key={index}>
-            <ProfileCard
-              name={member.name}
-              title={member.title}
-              image={member.image}
-              socialLinks={member.socialLinks}
-            />
+        {isLargeScreen ? (
+          <Grid
+            container
+            justifyContent="center"
+            spacing={{ xs: 3, md: 7.5 }} // 24px for xs, 60px for md and up
+            columns={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {teamMembers.map((member, index) => (
+              <Grid item xs={1} key={index}>
+                <Box>
+                  <MyProfileCard
+                    name={member.name}
+                    title={member.title}
+                    image={member.image}
+                    socialLinks={member.socialLinks}
+                  />
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: {
+                xs: '1fr',
+                sm: 'repeat(2, 1fr)',
+                md: 'repeat(3, 1fr)',
+              },
+              gap: { xs: '40px', sm: '50px', md: '60px' },
+              justifyContent: 'center',
+              '& > *:last-child:nth-child(3n - 1)': {
+                gridColumn: {
+                  md: '2 / 3',
+                },
+              },
+              '& > *:last-child:nth-child(3n - 2)': {
+                gridColumn: {
+                  md: '2 / 3',
+                },
+              },
+            }}
+          >
+            {teamMembers.map((member, index) => (
+              <Box
+                key={index}
+                sx={{
+                  maxWidth: isSmallMobile ? '300px' : 'none',
+                  margin: '0 auto',
+                  width: '100%',
+                }}
+              >
+                <MyProfileCard
+                  name={member.name}
+                  title={member.title}
+                  image={member.image}
+                  socialLinks={member.socialLinks}
+                />
+              </Box>
+            ))}
           </Box>
-        ))}
+        )}
       </Box>
     </Box>
   );
